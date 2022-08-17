55cabb8f217f96c28f396aee839abe3bf73e9ae8
Teal Larson <teal.larson@grouparoo.com>
2022-08-16 21:54:57 -0400

    add meet the bytes interview to talks page

--- data/talksData.ts
+++ data/talksData.ts
@@ -1,7 +1,15 @@
 const talksData = [
   {
+    title: 'From Teacher to Software Engineer: A Real-Life Success Story | Meet The Bytes',
+    date: '08-11-2022',
+    description:
+      "A Meet the Bytes Interview with Chris Sean.  Learn about my path into tech, startup life, and what it's like to work at Airbyte.",
+    imgSrc: '/static/images/talks/2022-08-11-meet-the-bytes.png',
+    href: 'https://www.youtube.com/watch?v=cUpKGfA-Ooo',
+  },
+  {
     title: "Pathlight Community AMA: 'All Experience is Good Experience for a Software Engineer'",
-    date: '03-31-2021',
+    date: '03-31-2022',
     description: `Community AMA session with the Pathlight Breaking into Tech community about my career transition into tech and my experiences working at an early stage startup while being early-in-career`,
     imgSrc: '/static/images/talks/2022-03-31-all-experience-is-good-experience.png',
     href: 'https://www.youtube.com/watch?v=OsJVgfjanPI',
